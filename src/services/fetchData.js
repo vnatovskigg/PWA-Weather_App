5e301bfc11de5edc28729c69ddede43358888e9b
vnatovskigg <49907900+vnatovskigg@users.noreply.github.com>
2020-08-25 15:39:12 +0300

    feat: main functionality complete

---
+++ src/services/fetchData.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const fetchData = async (query) => {
+  const res = await fetch(
+    `${process.env.REACT_APP_API_URL}?q=${query}&appid=${process.env.REACT_APP_API_KEY}`
+  );
+
+  const data = await res.json();
+  return data;
+};
